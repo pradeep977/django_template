f3d92dee44a430a36a2e4f22b12382e6fa9be1c7
Pradeep <pradeep31219990@gmail.com>
2024-02-04 15:05:52 +0530

    add postgresql db connection

--- backend/config/local.py.example.py
+++ backend/config/local.py.example.py
@@ -1,1 +1,10 @@
 DEBUG = True
+
+# db config
+
+PG_HOST='localhost'
+PG_PORT=5432
+PG_USER='postgres'
+PG_PASSWORD='postgres'
+PG_NAME='dj_temp'
+
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': PG_NAME,
+        'USER': PG_USER,
+        'PASSWORD': PG_PASSWORD,
+        'HOST': PG_HOST,
+        'PORT': 5432,
     }
 }
 
